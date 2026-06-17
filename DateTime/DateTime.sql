1be6848e239857f45643b4769c42817edc0a7222
shrishti-04 <shrishtisreyansh@gmail.com>
2026-06-17 06:02:37 +0530

    DateGroupBy

--- DateTime/DateTime.sql
+++ DateTime/DateTime.sql
@@ -1,6 +1,7 @@
 USE SalesDB;
 
-SELECT *,
+SELECT OrderId,
+CreationTime,
 YEAR(CreationTime) AS YEARS,
 MONTH(CreationTime) AS MONTHS,
 DAY(CreationTime) AS DAYS,
@@ -14,5 +15,26 @@ DATETRUNC(HOUR, CreationTime) AS Reset_minsSecs,
 DATETRUNC(DAY, CreationTime) AS Reset_Time,
 DATETRUNC(MONTH, CreationTime) AS ResetDay,
 DATETRUNC(YEAR, CreationTime) AS ResetMonth,
-DATETRUNC(HOUR, CreationTime) AS Reset_minsSecs
+DATETRUNC(HOUR, CreationTime) AS Reset_minsSecs,
+EOMONTH(CreationTime) AS EndOfMonth,
+CAST(DATETRUNC(MONTH, CreationTime) AS DATE) AS StartOfMonth
 FROM Sales.Orders
+
+-- How many orders were placed each year?
+
+SELECT YEAR(OrderDate) AS YEARS, COUNT(OrderID) AS OrderCount FROM Sales.Orders
+GROUP BY YEAR(OrderDate)
+
+-- How many orders were placed each month?
+
+SELECT MONTH(OrderDate) AS Months, COUNT(OrderID) AS OrderCounts FROM Sales.Orders
+GROUP BY MONTH(OrderDate)
+ORDER BY COUNT(OrderID) DESC;
+
+-- Show all orders that were placed during the month of February
+
+SELECT OrderDate, OrderID FROM Sales.Orders
+WHERE MONTH(OrderDate) = 2;
+
+SELECT * FROM Sales.Orders
+WHERE MONTH(OrderDate) = 2;
