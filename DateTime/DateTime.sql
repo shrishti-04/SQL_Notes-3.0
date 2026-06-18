8a19c278ad952e29dedd0eca1a75a667a461177a
shrishti-04 <shrishtisreyansh@gmail.com>
2026-06-18 06:02:22 +0530

    DateTimeFormat

--- DateTime/DateTime.sql
+++ DateTime/DateTime.sql
@@ -38,3 +38,11 @@ WHERE MONTH(OrderDate) = 2;
 
 SELECT * FROM Sales.Orders
 WHERE MONTH(OrderDate) = 2;
+
+-- Show CreationTime using the following format
+-- Day Wed Jan Q1 2025 12:34:56 PM
+
+SELECT OrderID,
+CreationTime,
+'Day ' + FORMAT(CreationTime, 'ddd MMM') + ' Q' + DATENAME(QUARTER, CreationTime) + ' ' + FORMAT(CreationTime, 'yyyy hh:mm:ss tt') FormatGiven
+FROM Sales.Orders
